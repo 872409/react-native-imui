e25bd068de3743926524cf23f20ca80fe3785b87
872409 <872409@qq.com>
2019-12-30 18:40:33 +0800

    avatar v1.2.11

--- ios/RCTAuroraIMUI/IMUIMessageCollectionView/Views/IMUIBaseMessageCell.swift
+++ ios/RCTAuroraIMUI/IMUIMessageCollectionView/Views/IMUIBaseMessageCell.swift
@@ -25,7 +25,7 @@ open class IMUIBaseMessageCell: UICollectionViewCell, IMUIMessageCellProtocal,Me
   weak var statusView: UIView?
    @objc public var cellType:String?
    @objc public var cellMsgId:String?
-    
+
   @objc weak var delegate: IMUIMessageMessageCollectionViewDelegate?
   var message: IMUIMessageModelProtocol?
     var cellGesture = UITapGestureRecognizer.init()
@@ -35,7 +35,7 @@ open class IMUIBaseMessageCell: UICollectionViewCell, IMUIMessageCellProtocal,Me
 
     bubbleView = IMUIMessageBubbleView(frame: CGRect.zero)
     super.init(frame: frame)
-    
+
     self.contentView.addSubview(self.bubbleView)
     self.contentView.addSubview(self.avatarImage)
     self.contentView.addSubview(self.timeBackView)
@@ -49,7 +49,7 @@ open class IMUIBaseMessageCell: UICollectionViewCell, IMUIMessageCellProtocal,Me
 //    self.bubbleView.isUserInteractionEnabled = true
 //    self.bubbleView.addGestureRecognizer(bubbleGesture)
 //    self.bubbleView.addGestureRecognizer(longPressBubbleGesture)
-    
+
     self.cellGesture.addTarget(self, action: #selector(self.tapCellView))
     self.bubbleGesture.addTarget(self, action: #selector(self.tapBubbleView))
     self.bubbleGesture.cancelsTouchesInView = false
@@ -59,26 +59,26 @@ open class IMUIBaseMessageCell: UICollectionViewCell, IMUIMessageCellProtocal,Me
     self.bubbleView.addGestureRecognizer(self.longPress)
     self.bubbleGesture.numberOfTapsRequired = 1
     self.addGestureRecognizer(self.cellGesture)
-    
+
     let avatarGesture = UITapGestureRecognizer(target: self, action: #selector(self.tapHeaderImage))
     avatarGesture.numberOfTapsRequired = 1
     avatarImage.isUserInteractionEnabled = true
     avatarImage.addGestureRecognizer(avatarGesture)
-    
+
     let longAvatarPress = UILongPressGestureRecognizer(target: self, action: #selector(self.longTapAvatarPress(sender:)))
     avatarImage.addGestureRecognizer(longAvatarPress)
     nameLabel.font = IMUIMessageCellLayout.nameLabelTextFont
     durationLabel.font = UIFont.systemFont(ofSize: 14)
     durationLabel.textColor = UIColor.init(red: 157/255.0, green: 157/255.0, blue: 158/255.0, alpha: 1)
-    
+
     isPlayedView.backgroundColor = UIColor.init(red: 216/255.0, green: 38/255.0, blue: 23/255.0, alpha: 1)
     isPlayedView.clipsToBounds = true
     isPlayedView.layer.cornerRadius = 3
-    
+
     self.setupSubViews()
 
   }
-  
+
   fileprivate func setupSubViews() {
     timeLabel.textAlignment = .center
     timeLabel.textColor = UIColor.white
@@ -87,42 +87,42 @@ open class IMUIBaseMessageCell: UICollectionViewCell, IMUIMessageCellProtocal,Me
     timeBackView.layer.cornerRadius = 5
     timeBackView.clipsToBounds = true
   }
-  
+
   required public init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  
+
   func layoutCell(with layout: IMUIMessageCellLayoutProtocal, viewCache: IMUIReuseViewCache) {
     self.timeBackView.frame = layout.timeLabelFrame
     self.timeLabel.frame = self.timeBackView.bounds
     self.avatarImage.frame = layout.avatarFrame
-    self.avatarImage.layer.cornerRadius = 5.0;
+    self.avatarImage.layer.cornerRadius = layout.avatarFrame.height*0.5;
     self.avatarImage.layer.masksToBounds = true;
     self.bubbleView.frame = layout.bubbleFrame
     self.nameLabel.frame = layout.nameLabelFrame
     self.durationLabel.frame = layout.durationLabelFrame
     self.isPlayedView.frame = layout.isPlayedFrame
     self.removeStatusView(viewCache: viewCache)
-    
+
     self.statusView = viewCache.statusViewCache.dequeue(layout: layout ) as? UIView
     self.contentView.addSubview(self.statusView!)
     self.addGestureForStatusView()
     self.nameLabel.textColor = UIColor.init(red: 157/255.0, green: 157/255.0, blue: 158/255.0, alpha: 1)
     self.statusView!.frame = layout.statusViewFrame
-    
+
   }
-  
+
   func addGestureForStatusView() {
     for recognizer in self.statusView?.gestureRecognizers ?? [] {
       self.statusView?.removeGestureRecognizer(recognizer)
     }
-    
+
     let statusViewGesture = UITapGestureRecognizer(target: self, action: #selector(self.tapSatusView))
     statusViewGesture.numberOfTapsRequired = 1
     self.statusView?.isUserInteractionEnabled = true
     self.statusView?.addGestureRecognizer(statusViewGesture)
   }
-  
+
   func removeStatusView(viewCache: IMUIReuseViewCache) {
     if let view = self.statusView {
       viewCache.statusViewCache.switchStatusViewToNotInUse(statusView: self.statusView as! IMUIMessageStatusViewProtocal)
@@ -136,7 +136,7 @@ open class IMUIBaseMessageCell: UICollectionViewCell, IMUIMessageCellProtocal,Me
       }
     }
   }
-  
+
   func setupData(with message: IMUIMessageModelProtocol) {
 //    self.avatarImage.image = message.fromUser.Avatar()
     self.avatarImage.setImageURL(message.fromUser.Avatar(), placeholderImage: "defaultHead")
@@ -166,7 +166,7 @@ open class IMUIBaseMessageCell: UICollectionViewCell, IMUIMessageCellProtocal,Me
     }
     self.durationLabel.isHidden = true
     self.isPlayedView.isHidden = true
-    
+
     let statusView = self.statusView as! IMUIMessageStatusViewProtocal
     self.statusView?.isHidden = false
     switch message.messageStatus {
@@ -204,32 +204,32 @@ open class IMUIBaseMessageCell: UICollectionViewCell, IMUIMessageCellProtocal,Me
       case .mediaDownloadFail:
         statusView.layoutMediaDownloadFail()
     }
-    
+
     if message.isOutGoing {
       self.nameLabel.textAlignment = .right
     } else {
       self.nameLabel.textAlignment = .left
     }
   }
-  
+
     func widthWithFont(font : UIFont,  text : String) -> CGFloat {
-        
+
         guard text.characters.count > 0  else {
-            
+
             return 0
         }
         let size = CGSize(width:CGFloat(MAXFLOAT), height:CGFloat(MAXFLOAT))
         let rect = text.boundingRect(with: size, options:.usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font : font], context:nil)
-        
+
         return rect.size.width+10
     }
-    
+
   func presentCell(with message: IMUIMessageModelProtocol, viewCache: IMUIReuseViewCache, delegate: IMUIMessageMessageCollectionViewDelegate?) {
     self.layoutCell(with: message.layout, viewCache: viewCache)
     self.setupData(with: message)
     self.delegate = delegate
   }
-  
+
   @objc func tapBubbleView() {
     UIApplication.shared.keyWindow?.endEditing(true)
     if self.message?.type == .text {
@@ -239,23 +239,23 @@ open class IMUIBaseMessageCell: UICollectionViewCell, IMUIMessageCellProtocal,Me
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "ShowOrigImageNotification"), object: strMsgID)
 //        self.delegate?.messageCollectionView?(didTapImageMessageBubbleInCell:rect, model: self.message! )
         self.delegate?.messageCollectionView?(didTapMessageBubbleInCell: self, model: self.message!)
-        
+
     }else if self.message?.type == .video && message?.mediaFilePath() != "" {
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "PlayVideoNotification"), object: message?.mediaFilePath())
         self.delegate?.messageCollectionView?(didTapMessageBubbleInCell: self, model: self.message!)
-        
+
     }else{
         self.delegate?.messageCollectionView?(didTapMessageBubbleInCell: self, model: self.message!)
     }
   }
-    
+
    @objc func tapCellView(){//点击整个cell，隐藏键盘
         self.delegate?.messageCollectionView?(tapCellView: "")
 //        UIApplication.shared.keyWindow?.endEditing(true)
     }
-    
-    
-    
+
+
+
   @objc func longTapBubbleView(sender : UILongPressGestureRecognizer) {
 
     if sender.state == UIGestureRecognizer.State.began && self.message?.messageStatus != .sending{
@@ -283,16 +283,16 @@ open class IMUIBaseMessageCell: UICollectionViewCell, IMUIMessageCellProtocal,Me
         self.delegate?.messageCollectionView?(longTapCellViewModel: self.message!)
     }
   }
-    
+
     public func popoverView(_ popoverView: MenuPopOverView!, didSelectItem strIndex: String!) {
         self.delegate?.messageCollectionView?(didShowMenuStr:strIndex, model: self.message!)
     }
-    
+
     public func popoverViewDidDismiss(_ popoverView: MenuPopOverView!) {
         let obj = "dismissMenu" as NSString
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "kClickLongTouchShowMenuNotification"), object: obj)
     }
-    
+
   @objc func tapHeaderImage() {
     self.delegate?.messageCollectionView?(didTapHeaderImageInCell: self, model: self.message!)
   }
@@ -301,19 +301,19 @@ open class IMUIBaseMessageCell: UICollectionViewCell, IMUIMessageCellProtocal,Me
         if !(self.message?.isOutGoing)! {
             if sender.state == UIGestureRecognizer.State.began{
                 self.delegate?.messageCollectionView?(longTapAvatarPressWithModel: self.message!)
-               
+
             }
         }
     }
-    
-    
+
+
   @objc func tapSatusView() {
     self.delegate?.messageCollectionView?(didTapStatusViewInCell: self, model: self.message!)
 //    let alter = UIAlertView.init(title: "重发该消息？", message: "", delegate: self, cancelButtonTitle: "取消", otherButtonTitles: "确定")
 //    alter.show()
-    
+
   }
-  
+
 //    public func alertView(_ alertView:UIAlertView, clickedButtonAt buttonIndex: Int){
 //        if(buttonIndex==alertView.cancelButtonIndex){
 //            print("取消")
@@ -321,17 +321,17 @@ open class IMUIBaseMessageCell: UICollectionViewCell, IMUIMessageCellProtocal,Me
 //            self.delegate?.messageCollectionView?(didTapStatusViewInCell: self, model: self.message!)
 //        }
 //    }
-    
+
   func didDisAppearCell() {
   }
 
-    
+
     deinit {
         NotificationCenter.default.removeObserver(self)
         self.bubbleView.removeGestureRecognizer(self.bubbleGesture)
         self.bubbleView.removeGestureRecognizer(self.longPress)
         self.removeGestureRecognizer(self.cellGesture)
     }
-    
+
 
 }
